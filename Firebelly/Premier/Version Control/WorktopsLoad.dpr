f15796b7f7d3ea5bb893f0c6da05dc8f0b021e96
Marcio Schumann dos Santos <m.schumanndossantos@gdksoftware.com>
2023-07-25 14:18:11 -0300

    Updating project files

--- Firebelly/Premier/Version Control/WorktopsLoad.dpr
+++ Firebelly/Premier/Version Control/WorktopsLoad.dpr
@@ -5,7 +5,10 @@ uses
   WorktopsVers in 'WorktopsVers.pas' {frmWorktopsVers},
   AllCommon in '..\..\AllCommon.pas',
   Office12_TLB in '..\..\Office12_TLB.pas',
-  Outlook12_TLB in '..\..\Outlook12_TLB.pas';
+  Outlook12_TLB in '..\..\Outlook12_TLB.pas',
+  Printer.Enums in '..\..\..\PrinterTools\Printer.Enums.pas',
+  Printer.Interfaces in '..\..\..\PrinterTools\Printer.Interfaces.pas',
+  Printer.Tools in '..\..\..\PrinterTools\Printer.Tools.pas';
 
 {$R *.RES}
 
